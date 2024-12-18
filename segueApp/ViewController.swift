949a766ddda33e0739c48af73164c6b6dd60b604
Busra Gazel <busragzl96@gmail.com>
2024-12-18 23:01:56 +0300

    Initial Commit

---
+++ segueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segueApp
+//
+//  Created by Musa on 18.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
